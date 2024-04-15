b6d7503939ff23e784151d4dd3ee08526942bcb7
rkarimbd <mdrezaulkhan@gmail.com>
2024-04-15 01:35:52 -0500

    final update

---
+++ DSMS/DTOs/AddressDto.cs
@@ -0,0 +1,12 @@
+﻿namespace DSMS.DTOs
+{
+    public record AddressDto
+    {
+        public int AddressId { get; init; }
+        public string Street { get; init; }
+        public string City { get; init; }
+        public string State { get; init; }
+        public string ZipCode { get; init; }
+    }
+
+}
---
+++ DSMS/DTOs/PatientDto.cs
@@ -0,0 +1,14 @@
+﻿namespace DSMS.DTOs
+{
+    public record PatientDto
+    {
+        public int PatientId { get; init; }
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public string ContactPhoneNumber { get; init; }
+        public string Email { get; init; }
+        public DateTime DateOfBirth { get; init; }
+        public AddressDto primaryAddress { get; init; }
+    }
+
+}
